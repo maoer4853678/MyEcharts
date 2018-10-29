98a1265df02fa638dee4c096b41688193335ae28
maoer5350008 <dongzhaoyu@goldwind.com.cn>
2018-10-29 12:58:59 +0800

    MyEcharts
    V1.0

---
+++ MyEcharts.py
@@ -0,0 +1,145 @@
+#coding=utf-8
+import os
+import json
+import numpy as np
+import pandas as pd
+import shutil
+
+#######################################################################
+## 通用函数
+def siplitlist(listx,n,axis = 0):
+    u'''
+    listx: 待分组序列，类型可以是list，np.array,pd.Series...
+    n: 若axis=0 n为组别个数,计算生成元素个数,若axis=1 n为每组元素个数,计算生成组别个数
+    axis: 若axis=0 按照固定组别个数分组, 若axis=1 按照固定元素个数分组
+    '''
+    if axis ==0:
+        a1,a2 = divmod(len(listx),n)
+        N = [a1]*(n-a2)+[a1+1]*a2 if a1!=0 else [1]*len(listx)
+        res = [listx[s:s+i] for s,i in enumerate(N) if len(listx[s:s+i])!=0]
+    else:
+        N = int(len(listx)/n)+1
+        res = [listx[i*n:(i+1)*n] for i in range(N) if len(listx[i*n:(i+1)*n])!=0]
+    return res
+    
+def get_template(template):
+    with open("template/%s.html"%template,"r") as  f:
+        template = f.read()
+    return template
+
+def creat_html(template,root,name):
+    ## name中的 特殊字符处理
+    for i in '? * : " < > \ / |'.split(' '):
+        name = name.replace(i,'')
+    name= name.strip()
+    if not os.path.exists(root):
+        os.makedirs(root)
+    jspath = os.path.join(os.path.dirname(root),"js")
+    if not os.path.exists(jspath): ## root 所在的根目录没有js文件夹，则将js 文件夹拷贝至此目录下
+        shutil.copytree('./js', jspath)
+    else: ## 若存在js文件不全的情况下单独拷贝文件
+        for i in os.listdir('./js'):
+            dstFilePath = os.path.join(jspath,i)
+            if not os.path.exists(dstFilePath):
+                shutil.copyfile('./js/'+i,dstFilePath)    
+    with open("%s/%s.html"%(root,name),"w") as  f:
+        f.write(template)
+
+#######################################################################
+## 时间序列的箱线图
+def box_map(root,name,alldata,xdata,lines):
+    template = get_template('box')
+    box = template%(json.dumps(alldata),json.dumps(xdata),json.dumps(lines))    
+    creat_html(box,root,name) 
+      
+def Plot_Box(df,x,y,kind='date',root='Time_series_analysis',filename = None):
+    u'''
+    按照指定时间窗口绘制 时序Box图，横轴是日期，可选指定变量
+    df: 类型 DataFrame
+    x: 横轴时间列，df[x].dtype 必须是 datetime64[ns]
+    y: 目标变量，df[y].dtype 必须是 int或者float
+    type: 时间窗口颗粒度 {date:每天 ,month:每月,year:每年} 默认每天
+    root: 离线网页生成所在目录
+    filename: 离线网页文件名称
+    Example:
+        df = pd.DataFrame(np.random.rand(70,4),columns = ['var1','var2','var3','var4'])
+        df['times'] = pd.date_range(start = '2018-01-02 00:00:00',freq = "1D",periods = len(df))
+        Plot_Box(df,'times','var1',kind = 'month')
+    '''
+    df[kind] = eval('df[x].dt.%s'%kind)
+    res = df.groupby(kind).apply(lambda x:x[y].values.tolist())
+    res =res.sort_index()
+    xdata = res.index.astype(str).tolist()
+    alldata = res.values.tolist()
+    lines = res.map(lambda x:np.percentile(x,50)).values.tolist()
+    if not filename:
+        filename = '%s_%s_box'%(kind,y)
+    box_map(root,filename,alldata,xdata,lines)
+#######################################################################
+## 单变量散点图
+def univariate_map(root,name,alldata,column):
+    template = get_template('univariate')
+    scatter = template%(json.dumps(alldata),json.dumps(column))
+    creat_html(scatter,root,name)
+        
+def Plot_Univariate(df,target,classf=None,varnum = 10,pagenum = None,root='Univariate_analysis',reverse =False):
+    u'''
+    Univariate分析
+    绘制单变量和目标变量的散点关系图，可以指定每页含有观察变量个数，按照变量顺序生成
+    df: 类型 DataFrame
+    target: 目标变量
+    classf: 可指定类别标签，若指定类别，将自动用颜色分类绘制
+    varnum: 在未指定pagenum的情况下，单页显示变量数量
+    pagenum: 生成网页数量，若指定pagenum，将优先按照pagenum 进行变量分组
+    root: 离线网页生成所在目录
+    Example:
+        df = pd.DataFrame(np.random.rand(50,4),columns = ['var1','var2','var3','target'])
+        df['class'] = ['A']*25+['B']*25
+        Plot_Univariate(df,'target','class')
+    '''
+    df.index = range(len(df))
+    temp = df.drop([target,classf],axis =1) if classf else df.drop([target],axis =1)
+    all_columns = temp.columns.tolist()
+    ## 根据varnum 和 pagenum 进行变量分组
+    columns = siplitlist(all_columns,pagenum,0) if pagenum else siplitlist(all_columns,varnum,1)
+    dfs = {}
+    if classf:
+        for key in df[classf].drop_duplicates():
+            dfs[str(key)] = df[df[classf]==key]
+    else :
+        dfs['all'] = df
+    
+    for index,column in enumerate(columns):
+        alldata ={}
+        ## column 为每页所需绘制的变量名称组
+        for col in column:
+            data= {}
+            for temp in dfs: ## temp 是按照classf 区分的不同组别数据
+                cols = [target,col] if reverse else [col,target]
+                data[temp] = dfs[temp][cols].values.tolist()
+            alldata[col] = data
+        
+        filename = 'Univariate_%d'%(index+1)
+        univariate_map(root,filename,alldata,column)
+
+def Plot_Scatter(df,x,y,root = "Scatter_analysis",name = "scatter",label =None):
+    dfs = {}
+    if label:
+        for key in df[label].drop_duplicates():
+            dfs[str(key)] = df[df[label]==key].values.tolist()
+    else :
+        dfs['all'] = df.values.tolist()
+        
+    template = get_template('scatter')
+    scatter = template%(json.dumps(dfs))
+    creat_html(scatter,root,name) 
+    
+if __name__ =="__main__":
+    df = pd.DataFrame(np.random.rand(50,4),columns = ['var1','var2','var3','target'])
+    df['class'] = ['A']*25+['B']*25
+    Plot_Univariate(df,'target','class',root = "ts/ts")
+    
+    
+    
+    
+    
